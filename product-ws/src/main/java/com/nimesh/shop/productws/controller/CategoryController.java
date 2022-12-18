da016700c5ce012f1d17136d0c3becb05e71af1f
Acer <Nimesh.garia@gmail.com>
2022-12-18 09:50:40 +0530

    feature-0.0.1: create products module added

--- product-ws/src/main/java/com/nimesh/shop/productws/controller/CategoryController.java
+++ product-ws/src/main/java/com/nimesh/shop/productws/controller/CategoryController.java
@@ -1,8 +1,15 @@
 package com.nimesh.shop.productws.controller;
 
+import com.nimesh.shop.productws.dto.response.BaseResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/category")
 public class CategoryController {
+    @PostMapping("/")
+    public ResponseEntity<BaseResponse> add
 
 }
